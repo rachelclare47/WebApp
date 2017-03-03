49b7261a86dadbc258fe192aa87653d5045d07c7
Darya <2198278S@student.gla.ac.uk>
2017-03-03 13:54:24 +0000

    Connected top rated and most listened page templates to home

--- WAD/ToP/urls.py
+++ WAD/ToP/urls.py
@@ -3,4 +3,7 @@ from ToP import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^top-rated/$', views.top_rated, name='top_rated'),
+    url(r'^most-listened/$', views.most_listened, name='most_listened'),
+    url(r'^playlist/$', views.show_playlist, name='show_playlist'),
 ]
--- WAD/ToP/views.py
+++ WAD/ToP/views.py
@@ -4,3 +4,13 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'ToP/home.html')
+
+def top_rated(request):
+    # create context_dict here to pass playlists sorted by rates into template
+    return render(request, 'ToP/top_rated.html')
+
+def most_listened(request):
+    return render(request, 'ToP/most_listened.html')
+
+def show_playlist(request):
+    return render(request, 'ToP/playlist.html')
