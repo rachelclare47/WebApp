9b2c39b553d7edfddb8030895b3bf2e8edd63694
Christopher Boland <2193014b@student.gla.ac.uk>
2017-03-06 12:10:36 +0000

    Login page renders properly

--- WAD/ToP/urls.py
+++ WAD/ToP/urls.py
@@ -3,9 +3,10 @@ from ToP import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^home/$', views.home, name='home'),
     url(r'^top-rated/$', views.top_rated, name='top_rated'),
     url(r'^most-listened/$', views.most_listened, name='most_listened'),
     url(r'^playlist/$', views.show_playlist, name='show_playlist'),
     url(r'^login/$', views.login, name='login'),
-	url(r'^register/$', views.register, name='register'),
+    url(r'^register/$', views.register, name='register'),
 ]
