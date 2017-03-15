32741d6ababd239ad5e995518e2309cb7baa0c1a
Christopher Boland <2193014b@student.gla.ac.uk>
2017-03-15 23:38:53 +0000

    Progress made on password reset & change - still not functional

--- WAD/ToP/forms.py
+++ WAD/ToP/forms.py
@@ -1,10 +1,14 @@
+from __future__ import unicode_literals
+
 from django import forms
 from django.contrib.auth.models import User
 from ToP.models import Playlist, Song, UserProfile
+from django.utils.http import urlsafe_base64_encode
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.sites.shortcuts import get_current_site
 
 
 class PlaylistForm(forms.ModelForm):
-<<<<<<< HEAD
     name = forms.CharField(max_length=128, help_text="Please enter the playlist name.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     rating = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
@@ -19,12 +23,10 @@ class PlaylistForm(forms.ModelForm):
         # Provide an association between the ModelForm and a model
         model = Playlist
         fields = ('name', 'picture', 'author',)
-=======
 
     class Meta:
         model=Playlist
         fields=('name','picture','author','views','rating','slug')
->>>>>>> aff045ad7040d4abad5ec5be52ecdf2ba99abe87
 
 
 class SongForm(forms.ModelForm):
@@ -51,3 +53,87 @@ class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ('picture',)
+
+
+class PasswordResetForm(forms.Form):
+    email = forms.EmailField(label ="Email", max_length=254)
+
+    def save(self, domain_override = None,
+             subject_template_name = 'registration/password_reset_subject.txt',
+             email_template_name = 'registration/password_reset_email.html',
+             use_https = False,
+             token_generator = default_token_generator,
+             from_email = None,
+             request = None,
+             html_email_template_name = None):
+        """
+        Generates a one-use only link for resetting the password and sends to the user.
+        """
+        from django.core.mail import send_mail
+        UserModel = get_user_model()
+        email = self.cleaned_data["email"]
+        active_users = UserModel._default_manager.filter(email__iexact=email, is_active=True)
+        for user in active_users:
+            # Make sure that no email is sent to any user that actually has a
+            # password marked as unusable.
+            if not user_has_usable_password():
+                continue
+            if not domain_override:
+                current_site = get_current_site(override)
+                site_name = current_site.name
+                domain = current_site.domain
+            else:
+                site_name = domain = domain_override
+            c = {
+                'email': email,
+                'domain': domain,
+                'site_name': site_name,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'user': user,
+                'token': token_generator.make_token(user),
+                'protocol': 'https' if user_https else 'http',
+            }
+            subject = loader.render_to_string(subject_template_name, c)
+            # Email subject must not contain new lines
+            subject = ''.join(subject.splitlines())
+            email = loader.render_to_string(email_template_name, c)
+
+            if html_email_template_name:
+                html_email = laoder.render_to_string(html_email_template_name, c)
+            else:
+                html_email = None
+            send_mail(subject, email, from_email, [user.email], html_message=html_email)
+
+
+class SetPasswordForm(forms.Form):
+    """
+    A form that lets a user change their password without entering the old password
+    """
+    error_messages = {
+        'password_mismatch': "The two password fields didn't match.",
+    }
+    new_password1 = forms.CharField(label="New Password",
+                                    widget=forms.PasswordInput)
+    new_password2 = forms.CharField(label="New Password Confirmation",
+                                    widget=forms.PasswordInput)
+
+    def __init__(self, user, *args, **kwargs):
+        self.user = user
+        super(SetPasswordForm, self).__init__(*args, **kwargs)
+
+    def clean_new_password2(self):
+        password1 = self.cleaned_data.get('new_password1')
+        password2 = self.cleaned_data.get('new_password2')
+        if password1 and password2:
+            if password1 != password2:
+                raise forms.ValidationError(
+                    self.error_messages['password_mismatch'],
+                    code = 'password_mismatch'
+                )
+        return password2
+
+    def save(self, commit=True):
+        self.user.set_password(slef.cleaned_data['new_password1'])
+        if commit:
+            self.user.save()
+        return self.user
--- WAD/ToP/views.py
+++ WAD/ToP/views.py
@@ -5,25 +5,37 @@ from ToP.models import Playlist, Song, User
 from django.contrib.auth import logout, authenticate
 from django.core.urlresolvers import reverse
 from datetime import datetime
+from django.template.response import TemplateResponse
+from django.utils.http import is_safe_url, urlsafe_base64_decode
+from django.shortcuts import resolve_url
+from django.views.decorators.cache import never_cache
+from django.contrib.auth import (REDIRECT_FIELD_NAME, login as auth_login,
+    logout as auth_logout, get_user_model, update_session_auth_hash)
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import PasswordResetForm, SetPasswordForm
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.sites.shortcuts import get_current_site
+from django.views.decorators.debug import sensitive_post_parameters
+from django.views.decorators.csrf import csrf_protect
+
 
 def home(request):
     return render(request, 'ToP/home.html')
+
 
 def top_rated(request):
     # create context_dict here to pass playlists sorted by rates into template
     return render(request, 'ToP/top_rated.html')
 
-<<<<<<< HEAD
-def most_listened(request):
-    return render(request, 'ToP/most_listened.html')
-
-=======
 
 def most_listened(request):
     return render(request, 'ToP/most_listened.html')
 
 
->>>>>>> aff045ad7040d4abad5ec5be52ecdf2ba99abe87
+def most_listened(request):
+    return render(request, 'ToP/most_listened.html')
+
+
 def most_viewed(request):
     return render(request, 'ToP/most_viewed.html')
 
@@ -210,12 +222,12 @@ def user_logout(request):
     return HttpResponseRedirect(reverse('home'))
 
 
-
 def get_server_side_cookie(request,cookie,default_val=None):
     val = request.session.get(cookie)
     if not val:
         val = default_val
     return val
+
 
 def visitor_cookie_handler(request):
     #Gets the number of views of the site
@@ -238,4 +250,119 @@ def visitor_cookie_handler(request):
     request.session['visits']=visits
 
 
-def password_change(request):
+@csrf_protect
+def password_reset(request,
+                   is_admin_site=False,
+                   template_name='registration/password_reset_form.html',
+                   email_template_name='registration/password_reset_email.html',
+                   subject_template_name = 'registration/password_reset_subject.txt',
+                   password_reset_form=PasswordResetForm,
+                   token_generator=default_token_generator,
+                   post_reset_redirect = None,
+                   from_email = None,
+                   current_app = None,
+                   extra_context = None,
+                   html_email_template_name = None):
+    if post_reset_redirect is None:
+        post_reset_redirect = reverse('password_reset_complete')
+    else:
+        post_reset_redirect = resolve_url(post_reset_redirect)
+    if request.method == 'POST':
+        form = password_reset_form(request.POST)
+        if form.is_valid():
+            opts = {
+                'use_https': request.is_secure(),
+                'token_generator': token_generator,
+                'from_email': from_email,
+                'email_template_name': email_template_name,
+                'subject_template_name': subject_template_name,
+                'request': request,
+                'html_email_template_name': html_email_template_name,
+            }
+            if is_admin_site:
+                opts = dict(opts, domain_override=request.get_host())
+            form.save(**opts)
+            return HttpResponseRedirect(post_reset_redirect)
+        else:
+            form = password_reset_form()
+        context = {
+            'form': form,
+            'title': _('Password reset'),
+        }
+        if extra_context is not None:
+            context.update(extra_context)
+        return HttpResponseRedirect(request, template_name, context)
+
+
+def password_reset_done(request,
+                        template_name = 'registration/password_reset_done.html',
+                        current_app = None,
+                        extra_context = None):
+    context = {
+        'title':_('Password reset successful'),
+    }
+    if extra_context is not None:
+        context.update(extra_context)
+    return TemplateResponse(request, template_name, context, currrent_app=current_app)
+
+
+@sensitive_post_parameters()
+@never_cache
+def passwored_reset_confirm(request,
+                            uidb64 = None,
+                            token = None,
+                            template_name = 'registration/password_reset_confirm.html',
+                            set_password_form = SetPasswordForm,
+                            token_generator = default_token_generator,
+                            post_reset_redirect = None,
+                            current_app = None,
+                            extra_context = None):
+    """View that checks the hash in a password reset link and presents a form for
+    entering a new password."""
+    UserModel = get_user_model()
+    assert uidb64 is not None and token is not None #checked by URLconf
+    if post_reset_redirect is None:
+        post_reset_redirect = reverse('password_reset_complete')
+    else:
+        post_reset_redirect = resolve_url(post_reset_redirect)
+    try:
+        uid = urlsafe_base64_decode(uidb64)
+        user = UserModle._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, UserModel.DoesNotExist):
+        user = None
+
+    if user is not None and token_generator.check_token(user, token):
+        validlink = True
+        title = _('Enter new password')
+        if request.method == 'POST':
+            form = set_password_form(user, request.POST)
+            if form.is_valid():
+                form.save()
+                return HttpResponseRedirect(post_reset_redirect)
+        else:
+            form = set_password_form(user)
+    else:
+        validlink = False
+        form  = None
+        title = _('Password reset unsuccessful')
+    context = {
+        'form': form,
+        'title': title,
+        'validlink': validlink
+    }
+    if extra_context is not None:
+        context.update(extra_context)
+    return TemplateResponse(request, template_name, context, current_app=current_app)
+
+
+def password_reset_complete(request,
+                            template_name='registration/password_reset_complete.html',
+                            current_app = None,
+                            extra_context = None):
+    context = {
+        'login_url': resolve_url(settings.LOGIN_URL),
+        'title': _('Password reset complete'),
+    }
+    if extra_context is not None:
+        context.update(extra_context)
+    return TemplateResponse(request, template_name, context, current_app=current_app)
