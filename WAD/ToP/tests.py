548207ba271ae951b0c22040b206a7e619aa4e19
Darya <2198278S@student.gla.ac.uk>
2017-03-24 11:24:13 +0000

    tests progress 2

--- WAD/ToP/tests.py
+++ WAD/ToP/tests.py
@@ -5,13 +5,16 @@ from ToP.views import *
 from ToP.urls import *
 from django.core.urlresolvers import reverse
 
+
 # Helper method to make a playlist
-def add_p(name, views, rating):
+def add_p(name, views, rating, author):
     p = Playlist.objects.get_or_create(name=name)[0]
     p.views = views
     p.rating = rating
+    p.author = author
     p.save()
     return p
+
 
 # Helper method to make a song
 def add_s(title, album, artist, genre):
@@ -21,6 +24,15 @@ def add_s(title, album, artist, genre):
     s.genre = genre
     s.save()
     return s
+
+
+# Helper method to make a user
+def add_user(username, email, password):
+    u = UserProfile.objects.get_or_create(user=username)[0]
+    u.email = email
+    u.password = password
+    u.save()
+    return u
 
 
 class PlaylistMethodTests(TestCase):
@@ -68,10 +80,10 @@ class TopRatedViewTests(TestCase):
     def test_top_rated_view_with_playlists(self):
         # Make sure that top rated page has playlists displayed
         # Create 4 playlists
-        add_p('test', 1, 1)
-        add_p('temp', 1, 1)
-        add_p('tmp', 1, 1)
-        add_p('tmp test temp', 1, 1)
+        add_p('test', 1, 1, 'wadteam1')
+        add_p('temp', 1, 1, 'wadteam1')
+        add_p('tmp', 1, 1, 'wadteam1')
+        add_p('tmp test temp', 1, 1, 'wadteam1')
 
         # Check that page loaded and that the response contains the last playlist created
         response = self.client.get(reverse('top_rated'))
@@ -87,7 +99,7 @@ class ShowPlaylistViewTests(TestCase):
     def test_show_playlist_without_songs(self):
         # If no songs exist in the context, display the appropriate message
         # Also tests that page loads successfully and that context_dict['songs'] is empty
-        add_p('test', 1, 1)
+        add_p('test', 1, 1, 'wadteam1')
         response = self.client.get('/ToP/playlist/test/')
         self.assertEquals(response.status_code, 200)
         self.assertContains(response, "No songs currently in playlist.")
@@ -95,6 +107,8 @@ class ShowPlaylistViewTests(TestCase):
 
     '''def test_show_playlist_with_songs(self):
         # Make sure that individual playlist page has songs displayed
+        # Create a playlist to put songs into
+        add_p('test', 1, 1)
         # Create 4 songs
         add_s('Tonite', 'Infinite', 'Eminem', 'Rap')
         add_s('Never 2 Far', 'Infinite', 'Eminem', 'Rap')
@@ -102,18 +116,128 @@ class ShowPlaylistViewTests(TestCase):
         add_s('Jealousy Woes II', 'Infinite', 'Eminem', 'Rap')
 
         # Check that page loaded and that the response contains the last song created
-        response = self.client.get(reverse('show_playlist'))
+        response = self.client.get('/ToP/playlist/test/')
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Jealousy Woes II')
 
         # Check that the context has 4 songs
         num_of_songs = len(response.context['songs'])
-        self.assertEqual(num_of_songs, 4)
+        self.assertEqual(num_of_songs, 4)'''
 
     def test_show_playlist_view_shows_view_count(self):
         # Make sure that there are views passed through context
+        dict = {'visits': 1}
+        add_p('test', 1, 1, 'wadteam1')
+        response = self.client.get('/ToP/playlist/test/')
+        self.assertEqual(response.context['visits'], dict['visits'])
+
+    def test_show_playlist_view_shows_average_rating(self):
+        # Make sure that there are ratings passed through context
+        add_p('test', 1, 1, 'wadteam1')
+        response = self.client.get('/ToP/playlist/test/')
+        self.assertContains(response, response.context['total_rating'])
+
+
+class MostViewedViewTests(TestCase):
+    def test_most_viewed_view_without_playlists(self):
+        # If no playlists exist, display the appropriate message
+        # Also tests that page loads successfully and that context_dict is empty
+        response = self.client.get(reverse('most_viewed'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "There are no playlists present.")
+        self.assertQuerysetEqual(response.context['playlists'], [])
+
+    def test_most_viewed_view_with_playlists(self):
+        # Make sure that top rated page has playlists displayed
+        # Create 4 playlists
+        add_p('test', 1, 1, 'wadteam1')
+        add_p('temp', 1, 1, 'wadteam1')
+        add_p('tmp', 1, 1, 'wadteam1')
+        add_p('tmp test temp', 1, 1, 'wadteam1')
+
+        # Check that page loaded and that the response contains the last playlist created
+        response = self.client.get(reverse('most_viewed'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'tmp test temp')
+
+        # Check that the context has 4 playlists
+        num_of_playlists = len(response.context['playlists'])
+        self.assertEqual(num_of_playlists, 4)
+
+'''class AddCommentViewTests(TestCase):
+    def test_comment_form_passed_to_template(self):
+        # Make sure that a form is passed through the context and page loads
         add_p('test', 1, 1)
-        response = self.client.get('ToP/playlist/test')
-        self.assertQuerysetEqual(response.context['visits'], [1])'''
+        response = self.client.get('/ToP/playlist/test/comment/')
+        self.assertEqual(response.status_code, 302)
+        self.assertQuerysetEqual(response.context['form'], form)'''
 
 
+class ViewAllPlaylistsTests(TestCase):
+    def test_view_all_view_without_playlists(self):
+        # If no playlists exist, display the appropriate message
+        # Also tests that page loads successfully and that context_dict is empty
+        response = self.client.get(reverse('view_all_playlists'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "There are no playlists present.")
+        self.assertQuerysetEqual(response.context['playlists'], [])
+
+    def test_view_all_view_with_playlists(self):
+        # Make sure that top rated page has playlists displayed
+        # Create 4 playlists
+        add_p('test', 1, 1, 'wadteam1')
+        add_p('temp', 1, 1, 'wadteam1')
+        add_p('tmp', 1, 1, 'wadteam1')
+        add_p('tmp test temp', 1, 1, 'wadteam1')
+
+        # Check that page loaded and that the response contains the last playlist created
+        response = self.client.get(reverse('view_all_playlists'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'tmp test temp')
+
+        # Check that the context has 4 playlists
+        num_of_playlists = len(response.context['playlists'])
+        self.assertEqual(num_of_playlists, 4)
+
+
+'''class MyPlaylistsTests(TestCase):
+    def test_my_playlists_has_correct_author(self):
+        add_p('test', 1, 1, 'wadteam1')
+        response = self.client.get(reverse('my_playlists'))
+        self.assertEqual(response.status_code, 302)
+        for p in response.context['playlists']:
+            self.assertEqual(p.author, 'wadteam1')
+        num_of_playlists = len(response.context['playlists'])
+        self.assertEqual(num_of_playlists, 1)'''
+
+class ToPUrlsTests(TestCase):
+    def test_password_change_loads_successfully(self):
+        response = self.client.get(reverse('password_change'))
+        self.assertEqual(response.status_code, 302)
+
+    def test_password_change_complete_loads_successfully(self):
+        response = self.client.get(reverse('password_change_done'))
+        self.assertEqual(response.status_code, 302)
+
+    def test_add_song_page_loads_successfully(self):
+        add_p('test', 1, 1, 'wadteam1')
+        response = self.client.get('/ToP/playlist/test/add_song/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_create_playlist_page_loads_successfully(self):
+        response = self.client.get(reverse('create_playlist'))
+        self.assertEqual(response.status_code, 302)
+
+    def test_my_playlists_page_loads(self):
+        response = self.client.get(reverse('my_playlists'))
+        self.assertEqual(response.status_code, 302)
+
+    def test_add_comment_page_loads(self):
+        add_p('test', 1, 1, 'wadteam1')
+        response = self.client.get('/ToP/playlist/test/comment/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_add_rating_page_loads(self):
+        add_p('test', 1, 1, 'wadteam1')
+        response = self.client.get('/ToP/playlist/test/rating/')
+        self.assertEqual(response.status_code, 302)
--- WAD/ToP/urls.py
+++ WAD/ToP/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     url(r'^my_playlists/accounts/password_change/$', auth_views.password_change, name='password_change'),
     url(r'^accounts/password_change/done/$', auth_views.password_change_done, name='password_change_done'),
     url(r'^view_all_playlists/$', views.view_all_playlists, name='view_all_playlists'),
-    url(r'playlist/(?P<playlist_name_slug>[\w\-]+)/comment/$', views.add_comment_to_playlist),
-    url(r'playlist/(?P<playlist_name_slug>[\w\-]+)/rating/$', views.add_rating),
+    url(r'playlist/(?P<playlist_name_slug>[\w\-]+)/comment/$', views.add_comment_to_playlist, name='add_comment'),
+    url(r'playlist/(?P<playlist_name_slug>[\w\-]+)/rating/$', views.add_rating, name='add_rating'),
 ]
 
